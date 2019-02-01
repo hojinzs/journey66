8f5959cb4053bb6b22119e62c7195b461b409643
Stevelee <stevelee@YS-A14ui-MacBookPro.local>
2019-02-01 14:29:28 +0900

    mainpage edit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,18 @@
 @section('title', 'Home')
 
 @push('scripts')
-
+<script>
+    let backgrounds = ['adult-backlit-bicycle-1522545.jpg','adult-adventure-asphalt-969679.jpg','asphalt-aspiration-clouds-215.jpg'];
+    document.addEventListener("DOMContentLoaded", function(){
+        let container = document.getElementById("main");
+        let random = randomItem(backgrounds);
+        container.style.backgroundImage = "url(/assets/"+random+")";
+        
+        function randomItem(a){
+            return a[Math.floor(Math.random() * a.length)];
+        };
+    });    
+</script>
 @endpush
 
 @push('css')
@@ -15,16 +26,18 @@
         html, body{
             height: 100vh;
             margin: 0;
+            font-size: 18px;
         }
     	.main-container{
             width: 100%;
-            display: table;
             height: 100%;
+            background-position: center center;
+            background-size: cover;
+            background-repeat: no-repeat;
         }
         .main-box{
-            text-align: center;
-            display: table-cell;
-            vertical-align: middle;
+            margin-left: 5%;
+            margin-top: 10%;
         }
         .a-target{
             position: relative;
@@ -37,33 +50,54 @@
             top: 0;
             left: 0;
         }
+
+        .opacity-box{
+            background-color: white;
+            opacity: .7;
+            color: black;
+            display: inline-block;
+            margin-bottom: 1em;
+            padding: 0.5em;
+        }
+        .opacity-box.link-box{
+            margin-left: 0.5em;
+        }
+
+        .link_header{
+            font-size: 1.5em;
+            font-weight: 700;
+        }
+
+        
     </style>
 @endpush
 
 @section('contents')
 
-    @component('components.TopMenu')
-    @endcomponent
-
-    <div class="main-container">
+    <div class="main-container" id="main">
+        @component('components.TopMenu')
+        @endcomponent
         <div class="main-box">
-            <div class="d-flex flex-row justify-content-center">
-                <div class="p-2">
-                    <h1>- Journey 66 -</h1>
+            <div class="">
+                <div class="opacity-box">
+                    <h1>Journey 66</h1>
+                    <p>write and share your wonderful journey</p>
                 </div>
             </div>
-            <div class="d-flex flex-row justify-content-center">
-                <div class="a-target p-2">
+            <div class="">
+                <div class="a-target opacity-box link-box">
                     <a class="a-wrapper" href="/write"></a>
-                    <i class="fas fa-edit fa-5x"></i>
-                    <h3>write journey</h3>
-                    <p>write new journey</p>
+                    <p><span class="link_header"><i class="fas fa-edit"></i> write journey</span></p>
+                    <p>write new journey  <span class="arrow-right"><i class="fas fa-chevron-circle-right"></i></span>
+                    </p>
                 </div>
-                <div class="a-target p-2">
+            </div>
+            <div class="">
+                <div class="a-target opacity-box link-box">
                     <a class="a-wrapper" href="/journey_shuffle"></a>
-                    <i class="fas fa-random fa-5x"></i>
-                    <h3>shuffle</h3>
-                    <p>read journey randomly</p>
+                    <p><span class="link_header"><i class="fas fa-random"></i> shuffle</span></p>
+                    <p> read journey randomly   <i class="fas fa-chevron-circle-right"></i></p>
+                    </p>
                 </div>
             </div>
         </div>
